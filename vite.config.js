996559d493b8967f366ec13f13690ec23515a9d4
Harokku <crenna.simone@gmail.com>
2025-04-19 23:01:26 +0200

    Add Tailwind CSS and its Vite plugin dependencies
    Integrated Tailwind CSS (v4.1.4) and @tailwindcss/vite (v4.1.4) into the project. Updated `package.json` and `pnpm-lock.yaml` to include related dependencies and their resolutions. This setup enables Tailwind integration with the existing Vite-powered build process.

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,30 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['node_modules/@testing-library/jest-dom/vitest'],
-    // if you have few tests, try commenting this
-    // out to improve performance:
-    isolate: false,
-  },
-  build: {
-    target: 'esnext',
-  },
-  resolve: {
-    conditions: ['development', 'browser'],
-  },
+    plugins: [
+        solidPlugin(),
+        tailwindcss(),
+    ],
+    server: {
+        port: 4000,
+    },
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: ['node_modules/@testing-library/jest-dom/vitest'],
+        // if you have few tests, try commenting this
+        // out to improve performance:
+        isolate: false,
+    },
+    build: {
+        target: 'esnext',
+    },
+    resolve: {
+        conditions: ['development', 'browser'],
+    },
 });
